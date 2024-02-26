1ac6243ac25d5f43a1d5d63a66fff41646e236a0
luhouyang <luhouyang@gmail.com>
2024-02-26 09:38:27 +0800

    less error

--- lib/services/crazy_rgb_usecase.dart
+++ lib/services/crazy_rgb_usecase.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:macrohard/utilities/my_colors.dart';
 
 class CrazyRGBUsecase extends ChangeNotifier {
-  late Timer _timer;
+  late Timer timer;
   int currentColorIndex = 0;
   Color currentColor = MyColors().gamingRGB[0];
 
   bool isCrazyMode = false;
 
   CrazyRGBUsecase() {
-    _timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+    timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
       currentColorIndex =
           (currentColorIndex + 60) % MyColors().gamingRGB.length;
       currentColor = MyColors().gamingRGB[currentColorIndex];
